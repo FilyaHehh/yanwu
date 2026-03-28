04beaf8b864e72893362ef98dd2233bd3c6b638a
Filya <filyahehh@gmail.com>
2026-03-28 17:47:27 +0300

    28032026

---
+++ scripts.js
@@ -0,0 +1,69 @@
+filterSelection("all")
+function filterSelection(c) {
+  var x, i;
+  x = document.getElementsByClassName("filterDiv");
+  if (c == "all") c = "";
+
+  for (i = 0; i < x.length; i++) {
+    sw3RemoveClass(x[i], "show");
+    if (x[i].className.indexOf(c) > -1) sw3AddClass(x[i], "show");
+  }
+}
+
+function sw3AddClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    if (arr1.indexOf(arr2[i]) == -1) {
+      element.className += " " + arr2[i];
+    }
+  }
+}
+
+function sw3RemoveClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    while (arr1.indexOf(arr2[i]) > -1) {
+      arr1.splice(arr1.indexOf(arr2[i]), 1);
+    }
+  }
+  element.className = arr1.join(" ");
+}
+
+var btnContainer = document.getElementById("myBtnContainer");
+var btns = btnContainer.getElementsByClassName("btn");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
+
+//"use strict";
+//
+//const $ = document.querySelector.bind(document);
+
+//const quiz =$(".quiz");
+//const warning =$('.warning');
+//const inputNext =$('.input_right');
+//let count = 0;
+//let underscore =0;
+
+
+
+//if (typeof questions !== 'undefined' && questions.length > 0){
+ // quiz.classList.remove('hidden');
+ // ShowQuestions(count);
+//} else {
+//  warning.classList.remove('hidden');
+//}
+
+//inputNext
+
+//function ShowQuestions(quiz){
+
+//}
